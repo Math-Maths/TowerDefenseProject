9f2be2035706da8d847a4df94130f6e03ff08fc8
Math-Maths <matheus_godoy97@hotmail.com>
2023-05-16 22:03:53 -0300

    Revert "Shatered enemy and spawn enemy fix"
    This reverts commit b6212cb450218d06d13ddd7f7ef0718aa158ccd1.

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -22,36 +22,19 @@ public class EnemySpawner : MonoBehaviour
 
         if (timeUntilNextSpawn <= 0)
         {
-            SpawnEnemy();
-        }
-    }
+            Vector3 randomPosition = new Vector3(Random.Range(-maxX, maxX), 0.6f, Random.Range(-maxZ, maxZ));
 
-    void SpawnEnemy()
-    {
+            // Verificar se a distância mínima entre a posição aleatória e o spawner é suficiente
+            float distance = Vector3.Distance(randomPosition, transform.position);
+            if (distance >= minDistance)
+            {
+                GameObject enemy = Instantiate(enemyPrefab, randomPosition, Quaternion.identity);
 
-        int numberSide = Random.Range(1,3);
-        float zPos = 0;
-
-        if(numberSide == 1)
-        {
-            zPos = maxZ;
-        }
-        else if(numberSide == 2)
-        {
-            zPos = -maxZ;
-        }
-
-        Vector3 randomPosition = new Vector3(Random.Range(-maxX, maxX), 0.6f, zPos);
-
-        // Verificar se a distância mínima entre a posição aleatória e o spawner é suficiente
-        float distance = Vector3.Distance(randomPosition, transform.position);
-        if (distance >= minDistance)
-        {
-            GameObject enemy = Instantiate(enemyPrefab, randomPosition, Quaternion.identity);
-            // Definir o projectile shooter como alvo do inimigo
-            Enemy enemyScript = enemy.GetComponent<Enemy>();
-            enemyScript.projectileShooter = projectileShooter;
-            timeUntilNextSpawn = spawnDelay;
+                // Definir o projectile shooter como alvo do inimigo
+                Enemy enemyScript = enemy.GetComponent<Enemy>();
+                enemyScript.projectileShooter = projectileShooter;
+                timeUntilNextSpawn = spawnDelay;
+            }
         }
     }
 }
