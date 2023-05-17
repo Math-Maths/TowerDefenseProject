4c78262b5dc78c849deae9943ab43f619daa7b09
Math-Maths <matheus_godoy97@hotmail.com>
2023-05-17 10:23:38 -0300

    Revert "Revert "Shatered enemy and spawn enemy fix""
    This reverts commit 9f2be2035706da8d847a4df94130f6e03ff08fc8.

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -22,19 +22,36 @@ public class EnemySpawner : MonoBehaviour
 
         if (timeUntilNextSpawn <= 0)
         {
-            Vector3 randomPosition = new Vector3(Random.Range(-maxX, maxX), 0.6f, Random.Range(-maxZ, maxZ));
+            SpawnEnemy();
+        }
+    }
 
-            // Verificar se a distância mínima entre a posição aleatória e o spawner é suficiente
-            float distance = Vector3.Distance(randomPosition, transform.position);
-            if (distance >= minDistance)
-            {
-                GameObject enemy = Instantiate(enemyPrefab, randomPosition, Quaternion.identity);
+    void SpawnEnemy()
+    {
 
-                // Definir o projectile shooter como alvo do inimigo
-                Enemy enemyScript = enemy.GetComponent<Enemy>();
-                enemyScript.projectileShooter = projectileShooter;
-                timeUntilNextSpawn = spawnDelay;
-            }
+        int numberSide = Random.Range(1,3);
+        float zPos = 0;
+
+        if(numberSide == 1)
+        {
+            zPos = maxZ;
+        }
+        else if(numberSide == 2)
+        {
+            zPos = -maxZ;
+        }
+
+        Vector3 randomPosition = new Vector3(Random.Range(-maxX, maxX), 0.6f, zPos);
+
+        // Verificar se a distância mínima entre a posição aleatória e o spawner é suficiente
+        float distance = Vector3.Distance(randomPosition, transform.position);
+        if (distance >= minDistance)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, randomPosition, Quaternion.identity);
+            // Definir o projectile shooter como alvo do inimigo
+            Enemy enemyScript = enemy.GetComponent<Enemy>();
+            enemyScript.projectileShooter = projectileShooter;
+            timeUntilNextSpawn = spawnDelay;
         }
     }
 }
